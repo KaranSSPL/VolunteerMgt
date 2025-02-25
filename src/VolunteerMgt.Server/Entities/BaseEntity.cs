9b0db36090edec5edbf4c19465078d3fd11d7d51
Karan Jangid <karanjangid80@gmail.com>
2025-02-26 00:12:38 +0530

    Add endpoints

---
+++ src/VolunteerMgt.Server/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace VolunteerMgt.Server.Entities;
+
+public class BaseEntity<Tkey>
+{
+    public Tkey Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public string CreatedBy { get; set; }
+    public string? ModifiedBy { get; set; }
+}
